1555599b9e79db1c00b35652818bf3ee3e2bbde4
Mirian <92062517+MirianCr@users.noreply.github.com>
2022-02-11 20:21:41 -0300

    Update imagens.js

--- imagens.js
+++ imagens.js
@@ -12,14 +12,14 @@ let somDaColisao;
 let somDoPonto;
 
 function preload(){
-  imagemDaEstrada = loadImage("material/estrada.png");
-  imagemDoAtor = loadImage("material/ator-1.png");
-  imagemCarro = loadImage("material/carro-1.png");
-  imagemCarro2 = loadImage("material/carro-2.png");
-  imagemCarro3 = loadImage("material/carro-3.png");
+  imagemDaEstrada = loadImage("estrada.png");
+  imagemDoAtor = loadImage("ator-1.png");
+  imagemCarro = loadImage("carro-1.png");
+  imagemCarro2 = loadImage("carro-2.png");
+  imagemCarro3 = loadImage("carro-3.png");
   imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3];
 
-  somDaTrilha = loadSound("material/sons/trilha.mp3");
-  somDaColisao = loadSound("material/sons/colidiu.mp3");
-  somDoPonto = loadSound("material/sons/pontos.wav");
+  somDaTrilha = loadSound("trilha.mp3");
+  somDaColisao = loadSound("colidiu.mp3");
+  somDoPonto = loadSound("pontos.wav");
 }
